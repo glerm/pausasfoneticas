1e97beca2850bd2904d1be4daa25bc32313ef3da
Glerm Soares <organismo@gmail.com>
2010-07-07 23:40:23 -0300

    soluçao incompleta de classes que geram todas as colunas

--- BGUI.py
+++ BGUI.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 from Tkinter import *
-from Tkinter import *
 import tkMessageBox
 import Tkinter
 
@@ -16,88 +15,116 @@ root.title("Tipos de Relatório")
 root
 var = IntVar()
 
+######## funcoes globais
 
-def callback():
+def donothing():
 	tkMessageBox.showinfo("Pronto!", "Os relatorios ja foram gerados e estao no diretorio " )
 
-MAINFRAME=Tkinter.Frame(root)
-MAINFRAME.pack(side=LEFT)
+##########################################################
 
+pesquisa=["Marcelo","Marbas","Ronas","Guana","Kotange","Kanaima"]
+controle=["Simone","Tininha","Thiago","Ricardo","Augusto","Manoel"]
 
-R1 = Radiobutton(MAINFRAME,text="Relatorio Por Amostra", variable=var, value=1,command=sel)
-R1.pack(anchor = W)
-
-R2 = Radiobutton(MAINFRAME, text="Média por Faixa Etária", variable=var, value=2,command=sel)
-R2.pack(anchor = W)
-
-R3 = Radiobutton(MAINFRAME, text="Média por Tipos de Histórias", variable=var, value=3,command=sel)
-R3.pack( anchor = W)
-
-R4 = Radiobutton(MAINFRAME, text="Média por Classe de palavra", variable=var, value=4,command=sel)
-R4.pack( anchor = W)
-
-R5 = Radiobutton(MAINFRAME, text="TODOS", variable=var, value=4,command=sel)
-R5.pack( anchor = W)
+cor1="#BFACC5"
+cor2="#BFBEA2"
+cor3="#FBBEA2"
+cor4="#BFAAA2"
+cor5="#00B3A0"
 
 
 
-FRAME=Tkinter.Frame(MAINFRAME)
-FRAME.pack()
-
-
-c = Tkinter.Button(FRAME,text="Gerar relatórios", relief="groove",activebackground="#ED2139", bg="darkred",fg="white",bd=4, command=callback)
-c.pack()
-
-
-
-F1=Tkinter.Frame(FRAME)
-F1.pack()
-
-
-a=Tkinter.Label(F1, text="Amostras Pesquisa")
-a.pack()
-
-
-scrollbar1 = Scrollbar(F1)
-scrollbar1.pack( side = RIGHT, fill=Y )
-
-Lb1 = Listbox(F1,selectmode=MULTIPLE,yscrollcommand = scrollbar1.set, exportselection=0)
-Lb1.insert(1, "Geisa")
-Lb1.insert(2, "Carlos")
-Lb1.insert(3, "Carla")
-Lb1.insert(4, "Alexandre")
-Lb1.insert(5, "Jean")
-Lb1.insert(6, "Dilma")
-
-Lb1.config(bg="#BFBEA2", font=("Helvetica",20,"bold"))
-
-Lb1.pack(side = RIGHT, fill = BOTH)
-scrollbar1.config( command = Lb1.yview )
-
-F2=Tkinter.Frame(FRAME)
-F2.pack()
-
-b=Tkinter.Label(F2, text="Amostras Controle")
-b.pack()
-
-scrollbar2= Scrollbar(F2)
-scrollbar2.pack( side = RIGHT, fill=Y )
-
-
-Lb2 = Listbox(F2,selectmode=MULTIPLE,yscrollcommand = scrollbar2.set, exportselection=0)
-Lb2.insert(1, "Geraldo")
-Lb2.insert(2, "Camila")
-Lb2.insert(3, "Daniel")
-Lb2.insert(4, "Monica")
-Lb2.insert(5, "João")
-Lb2.insert(6, "Marina")
-
-Lb2.pack(side = RIGHT, fill = BOTH)
-Lb2.config(bg="#B2A9FF", font="Helvetica")
-scrollbar2.config( command = Lb1.yview )
+	
+#def enche_listbox(lista,lbox):	
+#	for i in lista:
+#		lbox.insert(len(lista), i)	
+		
 
 
 
+######### funcao que define a celula de itens com botoes de seleciona tudo/nada e todos itens dentro
+
+
+class Celula:
+	def __init__(self,root,coluna,listadeitens,tipo,cor):
+
+		F1=Tkinter.Frame(root)
+		F1.grid(row=1,column=coluna)
+
+
+		label=Tkinter.Label(F1, text=tipo)
+		label.pack()
+
+		scrollbar1 = Scrollbar(F1)
+		scrollbar1.pack( side = RIGHT, fill=Y )
+
+		lbox = Listbox(F1,selectmode=MULTIPLE,yscrollcommand = scrollbar1.set, exportselection=0)
+
+		for i in listadeitens:
+			lbox.insert(len(listadeitens), i)
+		tamanhodalista = len(listadeitens)
+
+		lbox.config(bg=cor, font=("Helvetica",12) )
+		lbox.pack(side = RIGHT, fill = BOTH)
+		scrollbar1.config( command = lbox.yview )
+
+
+
+		def ativarTUDO():
+			lbox.selection_set(0,tamanhodalista)
+
+		def desativarTUDO():
+			lbox.selection_clear(0,tamanhodalista)
+
+		
+
+			
+
+
+		Bframe1=Tkinter.Frame(root)
+		Bframe1.grid(row=2,column=coluna)
+		btn1a = Tkinter.Button(Bframe1,text="Todos", relief="groove",activebackground="#ED2139", 	            bg="darkred",fg="white",bd=4, command=ativarTUDO)
+		btn1a.grid(row=1,column=1)
+		btn1b = Tkinter.Button(Bframe1,text="Limpa", relief="groove",activebackground="#ED3921", 	bg="brown",fg="white",bd=4, command=desativarTUDO)
+		btn1b.grid(row=1,column=2)
+
+	self.itens= [lbox.get(i) for i in lbox.curselection()]
+
+
+
+
+
+
+
+
+a=Celula(root,1,pesquisa,"Pesquisa",cor1)
+b=Celula (root,2,controle,"Controle",cor2)
+c=Celula (root,3,controle,"Faixa etária",cor3)
+d=Celula (root,4,controle,"Tipo de História",cor4)
+e=Celula (root,5,controle,"Classe de Palavra",cor5)
+
+
+
+
+################### MENU
+
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Faixa Etaria", command=donothing)
+filemenu.add_command(label="Tipo de História", command=donothing)
+filemenu.add_command(label="Classe de Palavra", command=donothing)
+
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="Medias", menu=filemenu)
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Gerar", command=donothing)
+helpmenu.add_command(label="About...", command=donothing)
+menubar.add_cascade(label="Gerar", menu=helpmenu)
+
+root.config(menu=menubar)
+
+################################
 
 
 
