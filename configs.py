514b99e4ebf3001b757aa7955030c61138013119
Glerm Soares <organismo@gmail.com>
2011-07-18 20:32:47 -0300

    adicionada pasta do instalador

--- configs.py
+++ configs.py
@@ -8,11 +8,11 @@ PYPATH= './'
 
 
 # importante: nao esquecer de colocar a barra no fim exemplo: /home/user/amostras/
-ROOTpath = homedir+'/fonoaudiologia/amostras_ruins/'
-#ROOTpath = homedir+'/fonoaudiologia/novas_amostras/'
+ROOTpath = homedir+'/fono/fonoaudiologia/amostras_ruins/'
+#ROOTpath = homedir+'/fono/fonoaudiologia/novas_amostras/'
 
-CSVpath = PYPATH+'csv/'
-
+#CSVpath = PYPATH+'csv/'
+CSVpath = homedir+'/fono/github/pausasfoneticas/csv/'
 
 #ROOTpath = homedir+'/amostras/'
 
